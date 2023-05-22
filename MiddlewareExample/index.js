13689df6ec7428e3321c8e7285f6c45e6a10a0aa
surya-nand <83575893+surya-nand@users.noreply.github.com>
2023-05-22 23:24:11 +0530

    body-parser

--- MiddlewareExample/index.js
+++ MiddlewareExample/index.js
@@ -1,10 +1,45 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+//Attaching or using middleware in the server
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+//Dummy data for understanding database concept
+
+const dummyData = [
+    {username: 'surya' , email: 'suryaanand10@gmail.com', password:"abcdef" }
+]
 
 app.get('/',(req,res)=>{
-    res.send("Middleware")
+    res.sendFile(__dirname+'/index.html')
+})
+
+//Redirecting login page to landing page
+app.post('/login',(req,res)=>{
+    //Checking the data from input with database
+    let Users =  dummyData.filter(user => user.email===(req.body.email) && user.password===(req.body.password))
+    let user = Users[0]
+    if(user){
+        res.redirect('/landingpage')
+    }
+    else{
+        res.redirect('/signuppage')
+
+    }
+    
+    console.log(req.body)
+})
+
+
+app.get('/landingpage',(req,res)=>{
+    res.send("Landing page")
+})
+
+app.get('/signuppage',(req,res)=>{
+    res.send("You have create an account to access this page")
 })
 
 app.listen(4000, ()=>{
